6acbca2fd4172cf5135441b229f1e6347b731ab3
conanak99 <huyhoang8a5@gmail.com>
2024-09-21 23:06:38 +0800

    fix migration lol

---
+++ migrations/0003_purple_warhawk.sql
@@ -0,0 +1,1 @@
+CREATE INDEX `created_at_idx` ON `history` (`created_at`);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,20 @@
 import { sql } from 'drizzle-orm';
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { index, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 import { ImageSize } from '@/app/types';
 
-export const history = sqliteTable('history', {
-  id: integer('id').primaryKey(),
-  prompt: text('prompt').notNull(),
-  image: text('image').notNull(),
-  size: text('size').$type<ImageSize>().notNull(),
-  createdAt: text('created_at')
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .notNull(),
-});
+export const history = sqliteTable(
+  'history',
+  {
+    id: integer('id').primaryKey(),
+    prompt: text('prompt').notNull(),
+    image: text('image').notNull(),
+    size: text('size').$type<ImageSize>().notNull(),
+    createdAt: text('created_at')
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+  },
+  (table) => ({
+    createdAtIndex: index('created_at_idx').on(table.createdAt),
+  })
+);
